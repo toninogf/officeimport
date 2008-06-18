1005383d044fdd24e87163b1e25f3a6f47a9c471
daning106 <daning106@1f1f8779-f34f-0410-abb2-75b2f53f1092>
2008-06-18 16:40:27 +0000

    start the office import project

---
+++ xwiki-plugin-officeimport/src/main/java/com/xpn/xwiki/plugin/officeimport/OfficeImportPluginApi.java
@@ -0,0 +1,91 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package com.xpn.xwiki.plugin.officeimport;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ConnectException;
+
+import com.artofsolving.jodconverter.DocumentConverter;
+import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.api.Api;
+
+public class OfficeImportPluginApi extends Api
+{
+    /**
+     * the plugin instance
+     */
+    private OfficeImportPlugin plugin;
+
+    public OfficeImportPluginApi(OfficeImportPlugin plugin, XWikiContext context)
+    {
+        super(context);
+        setPlugin(plugin);
+    }
+
+    public OfficeImportPlugin getPlugin()
+    {
+        if (hasProgrammingRights()) {
+            return plugin;
+        }
+        return null;
+    }
+
+    public void setPlugin(OfficeImportPlugin plugin)
+    {
+        this.plugin = plugin;
+    }
+
+    public String getMyHelloworld()
+    {
+        return "hello world!";
+    }
+
+    public String getMyHelloworld(String name)
+    {
+        return "hello " + name;
+    }
+
+    public OutputStream convert(InputStream inputDoc)
+    {
+        File inputFile = new File("document.doc");
+        File outputFile = new File("document.pdf");
+        OpenOfficeConnection connection = new SocketOpenOfficeConnection(8100);
+        try {
+            connection.connect();
+        } catch (ConnectException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // convert
+        DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
+        converter.convert(inputFile, outputFile);
+         
+        // close the connection
+        connection.disconnect();
+
+        return null;
+    }
+}
